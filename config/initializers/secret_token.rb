3c99303316e929e271beffe068b30a7e8bdf12e0
kumakoh <kumakoh@gmail.com>
2011-08-19 21:40:55 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Obsidian::Application.config.secret_token = '33d70d84ff46fb233eb7ee1859f83d5ce8fc7fa1e21afff1fb1200ff193b2a45e1b354a4067ee3881e5a253ac6906b39c2c29c01e978fddca853d32a5b6bd7da'
